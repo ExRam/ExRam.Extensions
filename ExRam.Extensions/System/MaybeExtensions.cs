07811b58a263505691d0e137f549ec7da0747e96
Blewzman <dw@exram.de>
2014-01-30 09:24:46 +0100

    Added MaybeExtensions.

---
+++ ExRam.Extensions/System/MaybeExtensions.cs
@@ -0,0 +1,10 @@
+﻿namespace System
+{
+    public static class MaybeExtensions
+    {
+        public static T? ToNullable<T>(this Maybe<T> maybe) where T : struct
+        {
+            return ((maybe.HasValue) ? (maybe.Value) : (new T?()));
+        }
+    }
+}
