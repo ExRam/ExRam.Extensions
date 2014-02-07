a523b8ca95002999161ffef839e024bd6dd618af
Blewzman <dw@exram.de>
2014-02-07 15:30:21 +0100

    New extension methods!

---
+++ ExRam.Extensions/System/Reactive/Linq/ObservableExtensions (CatchOnCancellation).cs
@@ -0,0 +1,24 @@
+﻿// (c) Copyright 2014 ExRam GmbH & Co. KG http://www.exram.de
+//
+// Licensed using Microsoft Public License (Ms-PL)
+// Full License description can be found in the LICENSE
+// file.
+
+using System.Diagnostics.Contracts;
+using System.Threading;
+
+namespace System.Reactive.Linq
+{
+    public static partial class ObservableExtensions
+    {
+        public static IObservable<T> CatchOnCancellation<T>(this IObservable<T> source, CancellationToken ct, IObservable<T> cancellationContinuation)
+        {
+            Contract.Requires(source != null);
+
+            return source.TakeUntil(ct)
+                .Concat(Observable.If(
+                    () => ct.IsCancellationRequested,
+                    cancellationContinuation));
+        }
+    }
+}
---
+++ ExRam.Extensions/System/Threading/CancellationTokenExtensions.cs
@@ -0,0 +1,26 @@
+﻿using System.Reactive;
+using System.Reactive.Linq;
+using System.Threading.Tasks;
+
+namespace System.Threading
+{
+    public static class CancellationTokenExtensions
+    {
+        public static Task ToTask(this CancellationToken ct)
+        {
+            var tcs = new TaskCompletionSource<object>();
+            ct.Register(() => tcs.SetResult(null));
+
+            return tcs.Task;
+        }
+
+        public static IObservable<Unit> ToObservable(this CancellationToken ct)
+        {
+            return Observable.Create<Unit>((observer) => ct.Register(() =>
+            {
+                observer.OnNext(Unit.Default);
+                observer.OnCompleted();
+            }));
+        }
+    }
+}
