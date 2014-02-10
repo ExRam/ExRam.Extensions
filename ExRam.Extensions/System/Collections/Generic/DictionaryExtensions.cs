84da9d7615d79b80d271b992352be5590f1b7d5c
Blewzman <dw@exram.de>
2014-02-10 12:13:18 +0100

    Added DictionaryExtensions.

---
+++ ExRam.Extensions/System/Collections/Generic/DictionaryExtensions.cs
@@ -0,0 +1,30 @@
+﻿using System.Diagnostics.Contracts;
+
+namespace System.Collections.Generic
+{
+    public static class DictionaryExtensions
+    {
+        public static TValue GetValueOrDefault<TKey, TValue>(this IDictionary<TKey, TValue> dictionary, TKey key)
+        {
+            Contract.Requires(dictionary != null);
+
+            return dictionary.GetValueOrDefault(key, default(TValue));
+        }
+
+        public static TValue GetValueOrDefault<TKey, TValue>(this IDictionary<TKey, TValue> dictionary, TKey key, TValue defaultValue)
+        {
+            Contract.Requires(dictionary != null);
+
+            TValue ret;
+            return ((dictionary.TryGetValue(key, out ret)) ? (ret) : (defaultValue));
+        }
+
+        public static Maybe<TValue> TryGetValue<TKey, TValue>(this IDictionary<TKey, TValue> dictionary, TKey key)
+        {
+            Contract.Requires(dictionary != null);
+
+            TValue item;
+            return ((dictionary.TryGetValue(key, out item)) ? (item) : (Maybe<TValue>.Null));
+        }
+    }
+}
