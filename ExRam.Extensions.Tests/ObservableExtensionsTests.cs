685f157e9556e297242546fc06fdc5ed23b2af79
Blewzman <dw@exram.de>
2014-02-21 16:17:51 +0100

    New: ObservableExtensions.SubscribeAtMost + unit test.

---
+++ ExRam.Extensions.Tests/ObservableExtensionsTests.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reactive.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace ExRam.Extensions.Tests
+{
+    [TestClass]
+    public class ObservableExtensionsTests
+    {
+        [TestMethod]
+        public void SubscribeAtMostTest()
+        {
+            var obs = Observable.Return(36)
+                .SubscribeAtMost(3);
+
+            for (var i = 0; i < 4; i++)
+            {
+                var called = false;
+
+                obs.Subscribe((v) => called = true);
+                
+                if (i < 3)
+                    Assert.IsTrue(called);
+                else
+                    Assert.IsFalse(called);
+            }
+        }
+    }
+}
