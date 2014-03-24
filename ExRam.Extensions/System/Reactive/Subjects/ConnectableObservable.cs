2176ce6202d733ce3ee8473256fb3b1de5f95b93
Blewzman <dw@exram.de>
2014-03-24 17:50:51 +0100

    New: ConnectableObservableExtensions.

---
+++ ExRam.Extensions/System/Reactive/Subjects/ConnectableObservable.cs
@@ -0,0 +1,42 @@
+﻿using System.Diagnostics.Contracts;
+
+namespace System.Reactive.Subjects
+{
+    public static class ConnectableObservable
+    {
+        #region FuncConnectableObservable
+        private sealed class FuncConnectableObservable<T> : IConnectableObservable<T>
+        {
+            private readonly Func<IDisposable> _connectFunction;
+            private readonly Func<IObserver<T>, IDisposable> _subscribeFuntion;
+
+            public FuncConnectableObservable(Func<IDisposable> connectFunction, Func<IObserver<T>, IDisposable> subscribeFuntion)
+            {
+                Contract.Requires(connectFunction != null);
+                Contract.Requires(subscribeFuntion != null);
+
+                this._connectFunction = connectFunction;
+                this._subscribeFuntion = subscribeFuntion;
+            }
+
+            public IDisposable Connect()
+            {
+                return this._connectFunction();
+            }
+
+            public IDisposable Subscribe(IObserver<T> observer)
+            {
+                return this._subscribeFuntion(observer);
+            }
+        }
+        #endregion
+
+        public static IConnectableObservable<T> Create<T>(Func<IDisposable> connectFunction, Func<IObserver<T>, IDisposable> subscribeFuntion)
+        {
+            Contract.Requires(connectFunction != null);
+            Contract.Requires(subscribeFuntion != null);
+
+            return new FuncConnectableObservable<T>(connectFunction, subscribeFuntion);
+        }
+    }
+}
---
+++ ExRam.Extensions/System/Reactive/Subjects/ConnectableObservableExtensions (DisconnectTotallyAtMost).cs
@@ -0,0 +1,45 @@
+﻿// (c) Copyright 2014 ExRam GmbH & Co. KG http://www.exram.de
+//
+// Licensed using Microsoft Public License (Ms-PL)
+// Full License description can be found in the LICENSE
+// file.
+
+using System.Diagnostics.Contracts;
+using System.Reactive.Linq;
+using System.Threading;
+
+namespace System.Reactive.Subjects
+{
+    public static partial class ConnectableObservableExtensions
+    {
+        public static IConnectableObservable<T> DisconnectTotallyAtMost<T>(this IConnectableObservable<T> source, int count, IObservable<T> continuation)
+        {
+            Contract.Requires(source != null);
+            Contract.Requires(count >= 0);
+            Contract.Requires(continuation != null);
+
+            var disconnectionCount = 0;
+
+            return ConnectableObservable.Create<T>(() =>
+            {
+                var connection = source.Connect();
+
+                return Disposables.Disposable.Create(() =>
+                {
+                    while (true)
+                    {
+                        var localConnectionCount = disconnectionCount;
+                        if (localConnectionCount >= count)
+                            return;
+
+                        if (Interlocked.CompareExchange(ref disconnectionCount, localConnectionCount + 1, localConnectionCount) == localConnectionCount)
+                        {
+                            connection.Dispose();
+                            return;
+                        }
+                    }
+                });
+            }, source.Subscribe);
+        }
+    }
+}
