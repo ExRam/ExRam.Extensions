513083e7c423ce3cefc23c69b417c20d1442f8b5
Blewzman <dw@exram.de>
2014-03-25 12:26:24 +0100

    Revised and added some tests.

--- ExRam.Extensions.Tests/ObservableExtensionsTests.cs
+++ ExRam.Extensions.Tests/ObservableExtensionsTests.cs
@@ -2,9 +2,12 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Reactive.Linq;
+using System.Reactive.Subjects;
 using System.Text;
 using System.Threading.Tasks;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Reactive.Disposables;
+using Moq;
 
 namespace ExRam.Extensions.Tests
 {
@@ -12,22 +15,53 @@ namespace ExRam.Extensions.Tests
     public class ObservableExtensionsTests
     {
         [TestMethod]
-        public void SubscribeAtMostTest()
+        public void SubscribeTotallyAtMostTest()
         {
-            var obs = Observable.Return(36)
-                .SubscribeTotallyAtMost(3);
+            var observableMock = new Mock<IObservable<object>>();
+            observableMock.Setup(x => x.Subscribe(It.IsAny<IObserver<object>>())).Returns(Disposable.Empty);
 
-            for (var i = 0; i < 4; i++)
+            var subscribeAtMostObservable = observableMock.Object.SubscribeTotallyAtMost(3, Observable.Empty<object>());
+
+            for (var i = 0; i < 6; i++)
             {
-                var called = false;
-
-                obs.Subscribe((v) => called = true);
-                
-                if (i < 3)
-                    Assert.IsTrue(called);
-                else
-                    Assert.IsFalse(called);
+                subscribeAtMostObservable.Subscribe();
             }
+
+            observableMock.Verify(x => x.Subscribe(It.IsAny<IObserver<object>>()), Times.Exactly(3));
+        }
+
+        [TestMethod]
+        public void ConnectTotallyAtMostTest()
+        {
+            var observableMock = new Mock<IConnectableObservable<object>>();
+            observableMock.Setup(x => x.Connect()).Returns(Disposable.Empty);
+
+            var connectAtMostObservable = observableMock.Object.ConnectTotallyAtMost(3);
+
+            for (var i = 0; i < 6; i++)
+            {
+                connectAtMostObservable.Connect();
+            }
+
+            observableMock.Verify(x => x.Connect(), Times.Exactly(3));
+        }
+
+        [TestMethod]
+        public void DisconnectTotallyAtMostTest()
+        {
+            var disposableMock = new Mock<IDisposable>();
+            var observableMock = new Mock<IConnectableObservable<object>>();
+            observableMock.Setup(x => x.Connect()).Returns(disposableMock.Object);
+
+            var connectAtMostObservable = observableMock.Object.DisconnectTotallyAtMost(3);
+
+            for (var i = 0; i < 6; i++)
+            {
+                connectAtMostObservable.Connect().Dispose();
+            }
+
+            observableMock.Verify(x => x.Connect(), Times.Exactly(6));
+            disposableMock.Verify(x => x.Dispose(), Times.Exactly(3));
         }
     }
 }
