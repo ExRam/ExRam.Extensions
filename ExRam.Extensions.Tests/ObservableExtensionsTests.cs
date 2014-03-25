23b01591b1347a3a5219441b89e27e3fe49cfe84
Blewzman <dw@exram.de>
2014-03-25 13:08:53 +0100

    New Extension method: ConnectAndForget.

--- ExRam.Extensions.Tests/ObservableExtensionsTests.cs
+++ ExRam.Extensions.Tests/ObservableExtensionsTests.cs
@@ -1,12 +1,8 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
+using System.Reactive.Disposables;
 using System.Reactive.Linq;
 using System.Reactive.Subjects;
-using System.Text;
-using System.Threading.Tasks;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
-using System.Reactive.Disposables;
 using Moq;
 
 namespace ExRam.Extensions.Tests
