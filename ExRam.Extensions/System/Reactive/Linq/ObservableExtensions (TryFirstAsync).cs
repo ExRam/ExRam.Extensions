3ec8f287b8162f881ce581cdbaa1a0688ff5debe
Blewzman <dw@exram.de>
2014-04-14 17:16:58 +0200

    New: ObservableExtensions.TryFirstAsync.

---
+++ ExRam.Extensions/System/Reactive/Linq/ObservableExtensions (TryFirstAsync).cs
@@ -0,0 +1,22 @@
+﻿// (c) Copyright 2014 ExRam GmbH & Co. KG http://www.exram.de
+//
+// Licensed using Microsoft Public License (Ms-PL)
+// Full License description can be found in the LICENSE
+// file.
+
+using System.Diagnostics.Contracts;
+
+namespace System.Reactive.Linq
+{
+    public static partial class ObservableExtensions
+    {
+        public static IObservable<Maybe<T>> TryFirstAsync<T>(this IObservable<T> source)
+        {
+            Contract.Requires(source != null);
+
+            return source
+                .Select(x => (Maybe<T>)x)
+                .FirstOrDefaultAsync();
+        }
+    }
+}
