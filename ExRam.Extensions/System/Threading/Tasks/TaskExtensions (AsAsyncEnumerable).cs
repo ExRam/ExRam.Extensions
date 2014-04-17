8cb3f10f4ea082cfb353a8f4eaaf951362979b97
Blewzman <dw@exram.de>
2014-04-17 09:40:00 +0200

    New: AsAsyncEnumerable. Converts Tasks of IEnumerable<T> to IAsyncEnumerable<T>.

---
+++ ExRam.Extensions/System/Threading/Tasks/TaskExtensions (AsAsyncEnumerable).cs
@@ -0,0 +1,24 @@
+﻿// (c) Copyright 2014 ExRam GmbH & Co. KG http://www.exram.de
+//
+// Licensed using Microsoft Public License (Ms-PL)
+// Full License description can be found in the LICENSE
+// file.
+
+using System.Collections.Generic;
+using System.Diagnostics.Contracts;
+using System.Linq;
+
+namespace System.Threading.Tasks
+{
+    public static partial class TaskExtensions
+    {
+        public static IAsyncEnumerable<T> AsAsyncEnumerable<T>(this Task<IEnumerable<T>> enumerableTask)
+        {
+            Contract.Requires(enumerableTask != null);
+
+            enumerableTask
+                .ToAsyncEnumerable()
+                .SelectMany(x => x.ToAsyncEnumerable());
+        }
+    }
+}
