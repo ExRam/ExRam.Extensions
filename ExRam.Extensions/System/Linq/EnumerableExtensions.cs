3bf5752288f3f70dc0ebd9700a8d14b372d4aea9
Blewzman <dw@exram.de>
2014-05-08 13:20:36 +0200

    New: EnumerableExtensions.

---
+++ ExRam.Extensions/System/Linq/EnumerableExtensions.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections.Generic;
+using System.Diagnostics.Contracts;
+
+namespace System.Linq
+{
+    public static class EnumerableExtensions
+    {
+        #region ForEach
+        public static void ForEach<T>(this IEnumerable<T> array, Action<T> action)
+        {
+            Contract.Requires(array != null);
+
+            foreach (var obj in array)
+            {
+                action(obj);
+            }
+        }
+        #endregion
+    }
+}
