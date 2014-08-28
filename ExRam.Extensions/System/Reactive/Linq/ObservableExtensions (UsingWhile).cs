8288bb6affdc347444f2e18f6ab226b7adaafda8
Daniel Weber <dw@exram.de>
2014-08-28 14:28:21 +0200

    New: UsingWhile.

---
+++ ExRam.Extensions/System/Reactive/Linq/ObservableExtensions (UsingWhile).cs
@@ -0,0 +1,42 @@
+﻿// (c) Copyright 2014 ExRam GmbH & Co. KG http://www.exram.de
+//
+// Licensed using Microsoft Public License (Ms-PL)
+// Full License description can be found in the LICENSE
+// file.
+
+using System.Diagnostics.Contracts;
+using System.Reactive.Disposables;
+
+namespace System.Reactive.Linq
+{
+    public static partial class ObservableExtensions
+    {
+        public static IObservable<TResult> UsingWhile<TResult, TResource>(Func<TResource> resourceFactory, Func<TResource, IObservable<TResult>> observableFactory, Predicate<TResult> predicate) where TResource : IDisposable
+        {
+            Contract.Requires(predicate != null);
+            Contract.Requires(resourceFactory != null);
+            Contract.Requires(observableFactory != null);
+
+            return Observable.Create<TResult>((observer) =>
+            {
+                var resource = resourceFactory();
+                var disposable = new SingleAssignmentDisposable
+                {
+                    Disposable = resource
+                };
+
+                return Observable
+                    .Return(Unit.Default)
+                    .SelectMany(unit => Observable.Using(
+                        () => disposable,
+                        _ => observableFactory(resource)))
+                    .Do(value =>
+                    {
+                        if ((!disposable.IsDisposed) && (!predicate(value)))
+                            disposable.Dispose();
+                    })
+                    .Subscribe(observer);
+            });
+        }
+    }
+}
