3dabf0c7dc44e61de1bf2a0dcee30ae481c115f8
Daniel Weber <dw@exram.de>
2015-01-19 01:56:45 +0100

    Update Ix.Async to version 1.2.3, remove now redundant ToAsyncEnumerable-method.

--- ExRam.Extensions/System/Threading/Tasks/TaskExtensions (ToAsyncEnumerable).cs
+++ ExRam.Extensions/System/Threading/Tasks/TaskExtensions (ToAsyncEnumerable).cs
@@ -13,25 +13,6 @@ namespace System.Threading.Tasks
 {
     public static partial class TaskExtensions
     {
-        public static IAsyncEnumerable<T> ToAsyncEnumerable<T>(this Task<T> task)
-        {
-            Contract.Requires(task != null);
-
-            return AsyncEnumerable2.Create(() =>
-            {
-                var called = false;
-
-                return AsyncEnumeratorEx.Create(async (ct) =>
-                {
-                    if (called)
-                        return Maybe<T>.Null;
-                 
-                    called = true;
-                    return await task.WithCancellation(ct);
-                });
-            });
-        }
-
         public static IAsyncEnumerable<Unit> ToAsyncEnumerable(this Task task)
         {
             Contract.Requires(task != null);
