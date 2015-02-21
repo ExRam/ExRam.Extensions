3491a2468ab07a7a0de1ae12820f3ba869e18ad3
Daniel Weber <dw@exram.de>
2015-02-21 18:42:16 +0100

    New: AsyncEnumerableExtensions.WithCancellation and ObservableExtensions.WithCancellation.

---
+++ ExRam.Extensions/System/Linq/AsyncEnumerableExtensions (WithCancellation).cs
@@ -0,0 +1,26 @@
+﻿// (c) Copyright 2014 ExRam GmbH & Co. KG http://www.exram.de
+//
+// Licensed using Microsoft Public License (Ms-PL)
+// Full License description can be found in the LICENSE
+// file.
+
+using System.Collections.Generic;
+using System.Diagnostics.Contracts;
+using System.Reactive.Disposables;
+using System.Threading;
+
+namespace System.Linq
+{
+    public static partial class AsyncEnumerableExtensions
+    {
+        public static IAsyncEnumerable<T> WithCancellation<T>(Func<CancellationToken, IAsyncEnumerable<T>> enumerableFactory)
+        {
+            Contract.Requires(enumerableFactory != null);
+
+            return AsyncEnumerable
+                .Using(
+                    () => new CancellationDisposable(),
+                    cts => enumerableFactory(cts.Token));
+        }
+    }
+}
