97bea543fe8c88275500b01c137d1e060d5d4672
Daniel Weber <dw@exram.de>
2015-09-21 09:40:17 +0200

    First version of a Prioritize-Operator.

---
+++ ExRam.Extensions/System/Reactive/Linq/ObservableExtensions (Prioritize).cs
@@ -0,0 +1,22 @@
+﻿// (c) Copyright 2014 ExRam GmbH & Co. KG http://www.exram.de
+//
+// Licensed using Microsoft Public License (Ms-PL)
+// Full License description can be found in the LICENSE
+// file.
+
+using System.Diagnostics.Contracts;
+
+namespace System.Reactive.Linq
+{
+    public static partial class ObservableExtensions
+    {
+        public static IObservable<T> Prioritize<T>(this IObservable<T> source, IObservable<T> other)
+        {
+            Contract.Requires(source != null);
+
+            return source
+                .Publish(publishedSource => publishedSource
+                    .Merge(other.TakeUntil(publishedSource)));
+        }
+    }
+}
