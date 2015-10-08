a4d08fe84ab247565b19b5937cbc07686e3003d8
Daniel Weber <dw@exram.de>
2015-10-08 09:35:38 +0200

    CancellationTokenExtensions.ToTask: That's in Nito.AsyncEx also, throw it out.

--- ExRam.Extensions/System/Threading/CancellationTokenExtensions.cs
+++ ExRam.Extensions/System/Threading/CancellationTokenExtensions.cs
@@ -1,19 +1,10 @@
 ﻿using System.Reactive;
 using System.Reactive.Linq;
-using System.Threading.Tasks;
 
 namespace System.Threading
 {
     public static class CancellationTokenExtensions
     {
-        public static Task ToTask(this CancellationToken ct)
-        {
-            var tcs = new TaskCompletionSource<object>();
-            ct.Register(() => tcs.SetResult(null));
-
-            return tcs.Task;
-        }
-
         public static IObservable<Unit> ToObservable(this CancellationToken ct)
         {
             return Observable
