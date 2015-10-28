09fface469ff11f46674713f2df26c7fb90424ec
Daniel Weber <dw@exram.de>
2015-10-28 11:11:51 +0100

    Add SelectMany-overload for observables for non-generic tasks. I can't believe this is missing.

---
+++ ExRam.Extensions/System/Reactive/Linq/ObservableExtensions (SelectMany).cs
@@ -0,0 +1,24 @@
+﻿// (c) Copyright 2014 ExRam GmbH & Co. KG http://www.exram.de
+//
+// Licensed using Microsoft Public License (Ms-PL)
+// Full License description can be found in the LICENSE
+// file.
+
+using System.Diagnostics.Contracts;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace System.Reactive.Linq
+{
+    public static partial class ObservableExtensions
+    {
+        public static IObservable<Unit> SelectMany<TSource>(this IObservable<TSource> source, Func<TSource, CancellationToken, Task> taskSelector)
+        {
+            Contract.Requires(source != null);
+            Contract.Requires(taskSelector != null);
+
+            return source
+                .SelectMany((x, ct) => taskSelector(x, ct).AsUnitTask());
+        }
+    }
+}
