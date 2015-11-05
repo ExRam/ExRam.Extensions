7ad9011d024ba5696797fd1a2c7c2c47521ddd36
Daniel Weber <dw@exram.de>
2015-11-05 08:14:29 +0100

    Reintroduce creation functions for AsyncEnumerables. Without them, some operators have a rather bad memory/time performance.

--- ExRam.Extensions/System/Linq/AsyncEnumerableExtensions (Create).cs
+++ ExRam.Extensions/System/Linq/AsyncEnumerableExtensions (Create).cs
@@ -6,17 +6,19 @@
 
 using System.Collections.Generic;
 using System.Diagnostics.Contracts;
+using System.Threading;
+using System.Threading.Tasks;
 
 namespace System.Linq
 {
     public static partial class AsyncEnumerableExtensions
     {
-        #region CreateFromEnumeratorFunctionAsyncEnumerable<T>
-        private sealed class CreateFromEnumeratorFunctionAsyncEnumerable<T> : IAsyncEnumerable<T>
+        #region FunctionAsyncEnumerable<T>
+        private sealed class FunctionAsyncEnumerable<T> : IAsyncEnumerable<T>
         {
             private readonly Func<IAsyncEnumerator<T>> _enumeratorCreationFunction;
 
-            public CreateFromEnumeratorFunctionAsyncEnumerable(Func<IAsyncEnumerator<T>> enumeratorCreationFunction)
+            public FunctionAsyncEnumerable(Func<IAsyncEnumerator<T>> enumeratorCreationFunction)
             {
                 Contract.Requires(enumeratorCreationFunction != null);
 
@@ -30,11 +32,92 @@ namespace System.Linq
         }
         #endregion
 
+        #region FunctionAsyncEnumerator<T>
+        private sealed class FunctionAsyncEnumerator<T> : IAsyncEnumerator<T>
+        {
+            private bool _isDisposed;
+            private readonly Action _disposeFunction;
+            private readonly Func<T> _currentFunction;
+            private readonly Func<CancellationToken, Task<bool>> _moveNextFunction;
+
+            private static readonly Task<bool> FalseTask = Task.FromResult(false);
+
+            public FunctionAsyncEnumerator(Func<CancellationToken, Task<bool>> moveNextFunction, Func<T> currentFunction, Action disposeFunction)
+            {
+                Contract.Requires(moveNextFunction != null);
+                Contract.Requires(currentFunction != null);
+                Contract.Requires(disposeFunction != null);
+
+                this._currentFunction = currentFunction;
+                this._disposeFunction = disposeFunction;
+                this._moveNextFunction = moveNextFunction;
+            }
+
+            public Task<bool> MoveNext(CancellationToken cancellationToken)
+            {
+                return this._isDisposed 
+                    ? FunctionAsyncEnumerator<T>.FalseTask 
+                    : this._moveNextFunction(cancellationToken);
+            }
+
+            public T Current => this._currentFunction();
+
+            public void Dispose()
+            {
+                if (!this._isDisposed)
+                {
+                    this._isDisposed = true;
+                    this._disposeFunction();
+                }
+            }
+        }
+        #endregion
+
         public static IAsyncEnumerable<T> Create<T>(Func<IAsyncEnumerator<T>> enumeratorCreationFunction)
         {
             Contract.Requires(enumeratorCreationFunction != null);
 
-            return new CreateFromEnumeratorFunctionAsyncEnumerable<T>(enumeratorCreationFunction);
+            return new FunctionAsyncEnumerable<T>(enumeratorCreationFunction);
+        }
+
+        public static IAsyncEnumerator<T> Create<T>(Func<CancellationToken, Task<bool>> moveNextFunction, Func<T> currentFunction, Action disposeFunction)
+        {
+            Contract.Requires(moveNextFunction != null);
+            Contract.Requires(currentFunction != null);
+            Contract.Requires(disposeFunction != null);
+
+            return new FunctionAsyncEnumerator<T>(moveNextFunction, currentFunction, disposeFunction);
+        }
+
+        public static IAsyncEnumerator<T> Create<T>(Func<CancellationToken, TaskCompletionSource<bool>, Task<bool>> moveNextFunction, Func<T> currentFunction, Action disposeFunction)
+        {
+            Contract.Requires(moveNextFunction != null);
+            Contract.Requires(currentFunction != null);
+            Contract.Requires(disposeFunction != null);
+
+            var ret = default(IAsyncEnumerator<T>);
+
+            ret = new FunctionAsyncEnumerator<T>(
+                async ct =>
+                {
+                    var tcs = new TaskCompletionSource<bool>();
+
+                    var cancel = new Action(() =>
+                    {
+                        ret.Dispose();
+                        tcs.TrySetCanceled();
+                    });
+
+                    using (ct.Register(cancel))
+                    {
+                        return await moveNextFunction(ct, tcs);
+                    }
+                },
+                currentFunction,
+                disposeFunction
+            );
+
+            return ret;
         }
     }
 }
