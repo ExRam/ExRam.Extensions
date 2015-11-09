b9ef41b194f8c2d3e50b27af2ac74a1580fbe500
Daniel Weber <dw@exram.de>
2015-11-09 09:42:22 +0100

    New: Using for Tasks, works like Using for Observables.

---
+++ ExRam.Extensions/System/Threading/Tasks/TaskExtensions (Using).cs
@@ -0,0 +1,25 @@
+﻿// (c) Copyright 2014 ExRam GmbH & Co. KG http://www.exram.de
+//
+// Licensed using Microsoft Public License (Ms-PL)
+// Full License description can be found in the LICENSE
+// file.
+
+using System.Diagnostics.Contracts;
+
+namespace System.Threading.Tasks
+{
+    public static partial class TaskExtensions
+    {
+        public async static Task<T> Using<T, TResource>(Func<TResource> resourceFactory, Func<TResource, Task<T>> taskFactory)
+            where TResource : IDisposable
+        {
+            Contract.Requires(resourceFactory != null);
+            Contract.Requires(taskFactory != null);
+
+            using (var res = resourceFactory())
+            {
+                return await taskFactory(res);
+            }
+        }
+    }
+}
