92e0671ba11da1c4dd446e61d46f1e857edac4cb
Daniel Weber <dw@exram.de>
2020-10-26 19:20:07 +0100

    Remove some ResultExtensions.

--- ExRam.Extensions/System/ResultExtensions.cs
+++ ExRam.Extensions/System/ResultExtensions.cs
@@ -8,30 +8,6 @@ namespace LanguageExt
     public static class ResultExtensions
     {
         [Pure]
-        public static Task<Result<B>> MapResult<A, B>(this Task<Result<A>> resultTask, Func<A, B> f)
-        {
-            return resultTask.Map(result => result.Map(f));
-        }
-
-        [Pure]
-        public static Task<Result<B>> BindResult<A, B>(this Task<Result<A>> resultTask, Func<A, Result<B>> f)
-        {
-            return resultTask.Map(result => result.Bind(f));
-        }
-
-        [Pure]
-        public static Task<Result<B>> MapResultAsync<A, B>(this Task<Result<A>> resultTask, Func<A, Task<B>> f)
-        {
-            return resultTask.Bind(result => result.MapAsync(f));
-        }
-
-        [Pure]
-        public static Task<Result<B>> BindResultAsync<A, B>(this Task<Result<A>> resultTask, Func<A, Task<Result<B>>> f)
-        {
-            return resultTask.Bind(result => result.BindAsync(f));
-        }
-
-        [Pure]
         public static Result<B> Bind<A, B>(this Result<A> result, Func<A, Result<B>> f)
         {
             return result.Match(
